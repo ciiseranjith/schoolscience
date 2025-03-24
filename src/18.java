1f3c395e69eb3a28407b125ba3c01026442c34de
ciiseranjith <ciiseranjith@gmail.com>
2025-03-24 17:36:03 +0000

    Add 18.java

---
+++ src/18.java
@@ -0,0 +1,21 @@
+public class SchoolScienceProject {
+    public static void main(String[] args) {
+        // Example of a simple Java program
+
+        System.out.println("Hello World!");
+
+        // Another example with variables
+        String name = "Alice";
+        int age = 18;
+        double height = 1.70;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+
+        // Another example with a loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}

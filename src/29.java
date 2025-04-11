5e626bd7c9ab5541bad8b8612fcd74903e13cde9
ciiseranjith <ciiseranjith@gmail.com>
2025-04-11 11:12:06 +0000

    Add 29.java

---
+++ src/29.java
@@ -0,0 +1,6 @@
+public class SchoolScienceProject {
+    public static void main(String[] args) {
+        // Code to be executed in this program goes here
+        System.out.println("This is some sample code.");
+    }
+}

400d3dce54b6bc9de57d23bdaf894c9b72ef2437
ciiseranjith <ciiseranjith@gmail.com>
2025-04-25 01:59:44 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,14 @@
+public class SchoolScienceProject {
+    public static void main(String[] args) {
+        // Code to be executed in the school science class project
+
+        System.out.println("Hello, this is your first Java program!");
+        
+        int a = 5;
+        double b = 3.14;
+        String c = "School Science Class";
+        boolean d = true;
+
+        // Add more code here if needed
+    }
+}

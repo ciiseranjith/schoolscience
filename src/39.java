4aa14927227e94b7898fa2e52970b3d1f6735b60
ciiseranjith <ciiseranjith@gmail.com>
2025-05-20 02:46:25 +0000

    Add 39.java

---
+++ src/39.java
@@ -0,0 +1,13 @@
+public class SchoolScienceProject {
+
+    public static void main(String[] args) {
+        // This is just an example of how to initialize some variables.
+        int age = 15;
+        String name = "Alice";
+
+        // Now you can use the variables in your code.
+        System.out.println("I am " + name);
+        System.out.println("My age is " + age);
+
+    }
+}

66c87b4230417398d1b314b87c0115f0f0759b1c
ciiseranjith <ciiseranjith@gmail.com>
2025-05-23 06:55:54 +0000

    Add 40.java

---
+++ src/40.java
@@ -0,0 +1,9 @@
+public class ScienceProject {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int x = 5;
+        double y = 3.14;
+        String name = "Alice";
+        System.out.println("Your name is: " + name);
+    }
+}

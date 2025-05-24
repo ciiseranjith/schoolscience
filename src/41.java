377a74c166f89394dde66bc51ad3464417ced3d7
ciiseranjith <ciiseranjith@gmail.com>
2025-05-24 17:04:16 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,6 @@
+public class SchoolScienceProject {
+    public static void main(String[] args) {
+        // Code to be executed in the main method of your project
+        System.out.println("Hello, World!");
+    }
+}
